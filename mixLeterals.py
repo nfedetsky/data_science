d3c6dfa2a6de31f4721a3eb35356df3993cd51d1
Nicholas Fedetsky <nfedetsky@gmail.com>
2022-10-26 16:09:17 +0300

    Перемешивание букв

---
+++ mixLeterals.py
@@ -0,0 +1,24 @@
+#Программа, перемешивающая буквы в слове
+#Вводим библиотеку random
+import random
+
+#Ввод слова
+str_1 = input('Введите слово: ')
+#Разбиваем слово на буквы
+strList = list(str_1)
+s = ' '
+#Перемешиваем буквы в списке
+result = random.sample(strList, len(strList))
+#Собираем из букв слово
+for i in range(0, len(result)):
+    s = s + result[i]
+print('Новое слово: ', s)
+
+s = input('Введите слово: ')
+
+str_1 = ' '
+
+for i in range(0, len(s)):
+    str_1 = str_1 + s[i] * 2
+
+print('Модификация слова: ', str_1)
