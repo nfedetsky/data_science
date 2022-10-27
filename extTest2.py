3e05896c2d86d6b83f56a26d9b56d6a240fd1ccc
Nicholas Fedetsky <nfedetsky@gmail.com>
2022-10-27 19:49:46 +0300

    Форматирование таблицы умножения

--- extTest2.py
+++ extTest2.py
@@ -1,44 +1,4 @@
-#Форматирование таблицы умножения
-#Ввод числа
-s = int(input('Введите любое целое число: '))
 
-if s:
-    #Задаем форматирование верхней таблици
-    if s < 1:
-        print('Вы ввели число меньше 1')
-    elif s > 1:
-        print('-' * (9 * s))
-        print('||', end='')
-        for i in range(1, s + 1):
-            if s > 9:
-                print('  %d    |' % (i), end='')
-            else:
-                print('  %d   |' % (i), end='')
-        print(' ')
-        print('-' * (9 * s))
-        #Цикл расчета таблицы умножения и формирвоание таблицы, куда выводим результат расчета
-        for k in range(1, s + 1):
-            print('|', end='')
-
-            for n in range(k, k * s + 1, k):
-
-                if n < 10:
-                    print('   %d    |' % (n), end='')
-
-                elif n < 100:
-                    print('   %d   |' % (n), end='')
-
-                elif n < 1000:
-                    print('  %d   |' % (n), end='')
-
-                else:
-                    print(' %d   |' % (n), end='')
-
-            print(' ')
-
-        print('_' * (9 * s))
-    else:
-        print('Введите число')
 
 
 
