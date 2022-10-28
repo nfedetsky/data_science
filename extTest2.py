1cdcfc881f669e432010c56e7991ab432fa7b5eb
Nicholas Fedetsky <nfedetsky@gmail.com>
2022-10-29 02:01:13 +0300

    Расчет работы инженеров

--- extTest2.py
+++ extTest2.py
@@ -1,4 +1,14 @@
 
+languages = [['English', 'Russian'], ['English'], ['English', 'German'], ['Chinese', 'Japanese']]
+person4_language2 = languages[3][1]
+print (person4_language2)
+languages_copy = [person[:] for person in languages] # [['English', 'Russian'], ['English'], ['English', 'German'], ['Chinese', 'Japanese']]
+print (languages_copy)
 
+i = 0
+for person in languages:
+    i += 1
+    for language in person:
+        print("Сотрудник #" + str(i) + " знает " + language)
 
 
